79f761def704921b2bb270edcea7367be2f718e5
Garrett Kegel <ghkegel@gmail.com>
2021-07-10 21:29:43 -0700

    Finished the rest of the remaining cases in the password generation so that the user can select any of the four options upper case, lower case, numbers, and special characters to include in their generated password.

--- script.js
+++ script.js
@@ -46,37 +46,37 @@ function generatePassword() {
     }
   }
 // Lower case prompt
-  lowerCasePrompt = prompt("Use lower case letters? Y or N");
-  if (lowerCasePrompt == "Y" || lowerCasePrompt == "y") {
+  lowerCasePrompt = confirm("Use lower case letters?");
+  if (lowerCasePrompt == true) {
     password.lowerCase = true;
-  } else if (lowerCasePrompt == "N" || lowerCasePrompt == "n") {
+  } else if (lowerCasePrompt == false) {
     password.lowerCase = false;
   } else {
     alert("Invalid entry, defaulting to true");
   }
   // Upper case prompt
-  upperCasePrompt = prompt("Use upper case letters? Y or N");
-  if (upperCasePrompt == "Y" || upperCasePrompt == "y") {
+  upperCasePrompt = confirm("Use upper case letters?");
+  if (upperCasePrompt == true) {
     password.upperCase = true;
-  } else if (upperCasePrompt == "N" || upperCasePrompt == "n") {
+  } else if (upperCasePrompt == false) {
     password.upperCase = false;
   } else {
     alert("Invalid entry, defaulting to true");
   }
   // Numbers prompt
-  numbersPrompt = prompt("Use numbers in password? Y or N");
-  if (numbersPrompt == "Y" || numbersPrompt == "y") {
+  numbersPrompt = confirm("Use numbers in password?");
+  if (numbersPrompt == true) {
     password.numbers = true;
-  } else if (numbersPrompt == "N" || numbersPrompt == "n") {
+  } else if (numbersPrompt == false) {
     password.numbers = false;
   } else {
     alert("Invalid entry, defaulting to true");
   }
   // Special characters prompt
-  specialsPrompt = prompt("Use special charactes in password? Y or N");
-  if (specialsPrompt == "Y" || specialsPrompt == "y") {
+  specialsPrompt = confirm("Use special characters in password?");
+  if (specialsPrompt == true) {
     password.specials = true;
-  } else if (specialsPrompt == "N" || specialsPrompt == "n") {
+  } else if (specialsPrompt == false) {
     password.specials = false;
   } else {
     alert("Invalid entry, defaulting to true");
@@ -131,37 +131,95 @@ function generatePassword() {
       }
   }
   // Sixth case : 1100
-  // else if () {
-
-  // }
+  else if (password.lowerCase == true && password.numbers == true &&
+    password.specials == false && password.upperCase == false) {
+      var totalChars = lowerAlphabet.concat(numberChars);
+      var passwordArray = [];
+      for (i=0; i < password.charLength; i++) {
+        passwordArray.push(totalChars[Math.floor(Math.random() * (totalChars.length - 1))]);
+      }
+  }
   // Seventh case : 1010
-  // else if () {
-
-  // }
-  // else if () {
-
-  // }
-  // else if () {
-
-  // }
-  // else if () {
-
-  // }
-  // else if () {
-
-  // }
-  // else if () {
-
-  // }
-  // else if () {
-
-  // }
-  // else if () {
-
-  // }
-  // else if () {
-
-  // }
+  else if (password.lowerCase == true && password.numbers == false &&
+    password.specials == true && password.upperCase == false) {
+      var totalChars = lowerAlphabet.concat(specialChars);
+      var passwordArray = [];
+      for (i=0; i < password.charLength; i++) {
+        passwordArray.push(totalChars[Math.floor(Math.random() * (totalChars.length - 1))]);
+      }
+  }
+  // Eighth case : 1001
+  else if (password.lowerCase == true && password.numbers == false &&
+    password.specials == false && password.upperCase == true) {
+      var totalChars = lowerAlphabet.concat(upperAlphabet);
+      var passwordArray = [];
+      for (i=0; i < password.charLength; i++) {
+        passwordArray.push(totalChars[Math.floor(Math.random() * (totalChars.length - 1))]);
+      }
+  }
+  // Ninth case : 0101
+  else if (password.lowerCase == false && password.numbers == true &&
+    password.specials == false && password.upperCase == true) {
+      var totalChars = numberChars.concat(upperAlphabet);
+      var passwordArray = [];
+      for (i=0; i < password.charLength; i++) {
+        passwordArray.push(totalChars[Math.floor(Math.random() * (totalChars.length - 1))]);
+      }
+  }
+  // Tenth case : 0011
+  else if (password.lowerCase == false && password.numbers == false &&
+    password.specials == true && password.upperCase == true) {
+      var totalChars = specialChars.concat(upperAlphabet);
+      var passwordArray = [];
+      for (i=0; i < password.charLength; i++) {
+        passwordArray.push(totalChars[Math.floor(Math.random() * (totalChars.length - 1))]);
+      }
+  }
+  // Eleventh case : 0110
+  else if (password.lowerCase == false && password.numbers == true &&
+    password.specials == true && password.upperCase == false) {
+      var totalChars = specialChars.concat(numberChars);
+      var passwordArray = [];
+      for (i=0; i < password.charLength; i++) {
+        passwordArray.push(totalChars[Math.floor(Math.random() * (totalChars.length - 1))]);
+      }
+  }
+  // Twelvth case : 1000
+  else if (password.lowerCase == true && password.numbers == false &&
+    password.specials == false && password.upperCase == false) {
+      var totalChars = lowerAlphabet;
+      var passwordArray = [];
+      for (i=0; i < password.charLength; i++) {
+        passwordArray.push(totalChars[Math.floor(Math.random() * (totalChars.length - 1))]);
+      }
+  }
+  // Thirteenth case : 0100
+  else if (password.lowerCase == false && password.numbers == true &&
+    password.specials == false && password.upperCase == false) {
+      var totalChars = numberChars;
+      var passwordArray = [];
+      for (i=0; i < password.charLength; i++) {
+        passwordArray.push(totalChars[Math.floor(Math.random() * (totalChars.length - 1))]);
+      }
+  }
+  // Fourteenth case : 0010
+  else if (password.lowerCase == false && password.numbers == false &&
+    password.specials == true && password.upperCase == false) {
+      var totalChars = specialChars;
+      var passwordArray = [];
+      for (i=0; i < password.charLength; i++) {
+        passwordArray.push(totalChars[Math.floor(Math.random() * (totalChars.length - 1))]);
+      }
+  }
+  // Fifteenth case : 0001
+  else if (password.lowerCase == false && password.numbers == false &&
+    password.specials == false && password.upperCase == true) {
+      var totalChars = upperAlphabet;
+      var passwordArray = [];
+      for (i=0; i < password.charLength; i++) {
+        passwordArray.push(totalChars[Math.floor(Math.random() * (totalChars.length - 1))]);
+      }
+  }
 
   var passwordStr = passwordArray.toString();
   var passwordStr1 = passwordStr.replace(/,/g, "");
